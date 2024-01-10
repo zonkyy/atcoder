a32144122d16e6305dbf899c8013abe2da5846ac
zonkyy <zonk.yy30@gmail.com>
2024-01-10 12:45:06 +0900

    b, c 解答

--- abc335/src/bin/b.rs
+++ abc335/src/bin/b.rs
@@ -1,3 +1,17 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: isize,
+    }
+
+    for x in 0..=n {
+        for y in 0..=n {
+            for z in 0..=n {
+                if x + y + z <= n {
+                    println!("{} {} {}", x, y, z);
+                }
+            }
+        }
+    }
 }
--- abc335/src/bin/c.rs
+++ abc335/src/bin/c.rs
@@ -1,3 +1,37 @@
+use std::collections::VecDeque;
+
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: isize,
+        q: usize,
+        queries: [(usize, String); q],
+    }
+
+    let mut head = (1, 0);
+    let mut history: VecDeque<(isize, isize)> = VecDeque::new();
+    for i in 1..=n {
+        history.push_back((i, 0));
+    }
+
+    for (t, x) in queries {
+        match t {
+            1 => {
+                match x.as_str() {
+                    "U" => head.1 += 1,
+                    "D" => head.1 -= 1,
+                    "L" => head.0 -= 1,
+                    "R" => head.0 += 1,
+                    _ => unreachable!(),
+                }
+                history.push_front(head);
+            }
+            2 => {
+                let node = history[x.parse::<usize>().unwrap() - 1];
+                println!("{} {}", node.0, node.1);
+            }
+            _ => unreachable!(),
+        }
+    }
 }
