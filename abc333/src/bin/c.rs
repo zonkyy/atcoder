542e0eb620613509349102b3736d56a8511e8613
zonkyy <zonk.yy30@gmail.com>
2024-01-22 15:55:15 +0900

    abc333 の c 回答

--- abc333/src/bin/c.rs
+++ abc333/src/bin/c.rs
@@ -1,3 +1,25 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+    };
+
+    let mut cnt = 0;
+
+    for d in 1..=13 {
+        for a in (0..=(d - 1)).rev() {
+            for b in (0..=(d - a - 1)).rev() {
+                cnt += 1;
+                if cnt == n {
+                    let c = d - a - b;
+                    let aa = vec!["1".to_string(); a];
+                    let bb = vec!["2".to_string(); b];
+                    let cc = vec!["3".to_string(); c];
+                    println!("{}{}{}", aa.join(""), bb.join(""), cc.join(""));
+                    return;
+                }
+            }
+        }
+    }
 }
