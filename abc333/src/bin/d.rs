1b37de57bd8c4e72b7d464a51519a9ee15da36f7
zonkyy <zonk.yy30@gmail.com>
2024-01-29 16:37:16 +0900

    abc333 の d と e を解答

--- abc333/src/bin/d.rs
+++ abc333/src/bin/d.rs
@@ -1,3 +1,40 @@
+use proconio::input;
+
+fn count_child(tree: &Vec<Vec<usize>>, node: usize, last: usize) -> usize {
+    let mut cnt = 0;
+    for i in tree[node].iter() {
+        if i != &last {
+            cnt += count_child(tree, *i, node);
+        }
+    }
+    cnt + 1
+}
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        uv: [(usize, usize); n-1],
+    };
+
+    let mut tree: Vec<Vec<usize>> = vec![vec![]; n];
+    for (u, v) in uv {
+        tree[u - 1].push(v - 1);
+        tree[v - 1].push(u - 1);
+    }
+
+    // 頂点 1 が葉なら 1 回
+    if tree[0].len() == 1 {
+        println!("1");
+        return;
+    }
+
+    // 頂点 1 が葉でないなら、頂点 1 の子の木のうち、最大のもののノード数の和 + 1 回
+    let mut counts: Vec<usize> = vec![];
+    for i in &tree[0] {
+        let cnt = count_child(&tree, *i, 0);
+        counts.push(cnt);
+    }
+    counts.sort();
+    counts.pop();
+    println!("{}", counts.iter().sum::<usize>() + 1);
 }
--- abc333/src/bin/e.rs
+++ abc333/src/bin/e.rs
@@ -1,3 +1,47 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        tx: [(usize, usize); n],
+    };
+
+    let mut battle = vec![0; 2 * n + 1];
+    let mut potion = Vec::new();
+    let mut cnt = 0;
+    let mut max = 0;
+
+    for (t, x) in tx.iter().rev() {
+        if t == &1 {
+            if battle[*x] > 0 {
+                battle[*x] -= 1;
+                cnt -= 1;
+                potion.push(1);
+            } else {
+                potion.push(0);
+            }
+        } else if t == &2 {
+            battle[*x] += 1;
+            cnt += 1;
+            if max < cnt {
+                max = cnt;
+            }
+        }
+    }
+
+    if battle.iter().all(|&x| x == 0) {
+        println!("{}", max);
+        println!(
+            "{}",
+            potion
+                .iter()
+                .rev()
+                .map(|x| x.to_string())
+                .collect::<Vec<String>>()
+                .join(" ")
+        );
+        return;
+    } else {
+        println!("-1");
+    }
 }
