a9411b4f6b7c853c5ac8d3419434f52faa2d1b7a
zonkyy <zonk.yy30@gmail.com>
2024-01-29 17:16:01 +0900

    abc332 の b を解答

--- abc332/src/bin/b.rs
+++ abc332/src/bin/b.rs
@@ -1,3 +1,23 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        (k, g, m): (usize, usize, usize),
+    };
+
+    let mut gn = 0;
+    let mut mn = 0;
+    for _ in 0..k {
+        if gn == g {
+            gn = 0;
+        } else if mn == 0 {
+            mn += m;
+        } else {
+            let pour = std::cmp::min(g - gn, mn);
+            gn += pour;
+            mn -= pour;
+        }
+    }
+
+    println!("{} {}", gn, mn);
 }
