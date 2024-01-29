222884eaa7fe608bbc117b3f0ed8da48eca208f5
zonkyy <zonk.yy30@gmail.com>
2024-01-29 17:05:09 +0900

    abc333 の f がわからないので modinv のみ実装

--- abc333/src/bin/f.rs
+++ abc333/src/bin/f.rs
@@ -1,3 +1,35 @@
+use proconio::input;
+
+fn modinv(a: i64, m: i64) -> i64 {
+    let mut a = a;
+    let mut b = m;
+    let mut u = 1;
+    let mut v = 0;
+
+    while b != 0 {
+        let t = a / b;
+        a -= t * b;
+        a ^= b;
+        b ^= a;
+        a ^= b;
+        u -= t * v;
+        u ^= v;
+        v ^= u;
+        u ^= v;
+    }
+
+    u %= m;
+    if u < 0 {
+        u += m;
+    }
+
+    u
+}
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+    };
+
+    println!("{}", 2 * modinv(3, 998244353));
 }
