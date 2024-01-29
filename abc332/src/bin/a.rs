8d857556da36b408e19de6e521551872e4d965da
zonkyy <zonk.yy30@gmail.com>
2024-01-29 17:11:28 +0900

    abc332 の a を解答

--- abc332/src/bin/a.rs
+++ abc332/src/bin/a.rs
@@ -1,3 +1,19 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        (n, s, k): (usize, usize, usize),
+        pq: [(usize, usize); n],
+    };
+
+    let mut sum = 0;
+    for (p, q) in pq {
+        sum += p * q;
+    }
+
+    if sum < s {
+        sum += k;
+    }
+
+    println!("{}", sum);
 }
