eca4fed58f036d5497d8884259aa91312097b6a6
zonkyy <zonk.yy30@gmail.com>
2024-04-01 15:34:00 +0900

    abc347 の c が 1 問間違い

--- abc347/src/bin/c.rs
+++ abc347/src/bin/c.rs
@@ -1,3 +1,20 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        a: usize,
+        b: usize,
+        mut d: [usize; n],
+    };
+
+    let d2 = d.iter().map(|x| x % (a + b)).collect::<Vec<usize>>();
+    let slide = d2.iter().min().unwrap();
+    let d3 = d2.iter().map(|x| x - slide).collect::<Vec<usize>>();
+
+    if d3.iter().max().unwrap() < &a {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
 }
