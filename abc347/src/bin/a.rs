da2f99aa8ddf163aa98ff7e85e41ef5deef80355
zonkyy <zonk.yy30@gmail.com>
2024-04-01 15:33:44 +0900

    abc347 の b まで解答

--- abc347/src/bin/a.rs
+++ abc347/src/bin/a.rs
@@ -1,3 +1,24 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        k: usize,
+        a: [usize; n],
+    };
+
+    let mut ans = Vec::new();
+    for i in a.iter() {
+        if i % k == 0 {
+            ans.push(i / k);
+        }
+    }
+
+    println!(
+        "{}",
+        ans.iter()
+            .map(|x| x.to_string())
+            .collect::<Vec<String>>()
+            .join(" ")
+    );
 }
--- abc347/src/bin/b.rs
+++ abc347/src/bin/b.rs
@@ -1,3 +1,16 @@
+use proconio::input;
+use std::collections::HashSet;
+
 fn main() {
-    todo!();
+    input! {
+        s: String,
+    };
+
+    let mut substrs = HashSet::new();
+    for l in 0..s.len() {
+        for r in l..s.len() {
+            substrs.insert(s.get(l..r + 1).unwrap());
+        }
+    }
+    println!("{:?}", substrs.len());
 }
