d9a2d8c838f24be7ab749ddd1f8ce2e3076b5ed4
zonkyy <zonk.yy30@gmail.com>
2024-04-04 16:29:16 +0900

    adt_easy_20240402_1 の d, e を解答

--- adt_easy_20240402_1/src/bin/d.rs
+++ adt_easy_20240402_1/src/bin/d.rs
@@ -2,7 +2,23 @@ use proconio::input;
 
 fn main() {
     input! {
-        n: usize,
-        a: [usize; n],
+        a: u64,
+        b: u64,
+        c: u64,
+        d: u64,
+        e: u64,
+        f: u64,
     };
+
+    const MOD: u64 = 998244353;
+    let mut left: u64 = ((a % MOD) * (b % MOD)) % MOD;
+    left = (left * (c % MOD)) % MOD;
+    let mut right: u64 = ((d % MOD) * (e % MOD)) % MOD;
+    right = (right * (f % MOD)) % MOD;
+
+    if left >= right {
+        println!("{}", left - right);
+    } else {
+        println!("{}", MOD + left - right);
+    }
 }
--- adt_easy_20240402_1/src/bin/e.rs
+++ adt_easy_20240402_1/src/bin/e.rs
@@ -1,8 +1,34 @@
+use itertools::Itertools;
 use proconio::input;
+use std::collections::HashMap;
 
 fn main() {
     input! {
         n: usize,
-        a: [usize; n],
+        k: usize,
+        s: [String; n],
     };
+
+    let mut ans = 0;
+    for i in 1..=n {
+        for comb in s.iter().combinations(i) {
+            let mut counter: HashMap<char, usize> = HashMap::new();
+            for c in comb
+                .iter()
+                .map(|s| s.as_str())
+                .collect::<Vec<_>>()
+                .join("")
+                .chars()
+            {
+                *counter.entry(c).or_insert(0) += 1;
+            }
+
+            let just_k = counter.values().filter(|&&v| v == k).count();
+            if just_k > ans {
+                ans = just_k;
+            }
+        }
+    }
+
+    println!("{}", ans);
 }
