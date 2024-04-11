c11173298eeb1728b8d9c512fc8bafa384f0e862
zonkyy <zonk.yy30@gmail.com>
2024-04-11 16:07:23 +0900

    adt_all_20240403_2 の f 解答

--- adt_all_20240403_2/src/bin/f.rs
+++ adt_all_20240403_2/src/bin/f.rs
@@ -4,6 +4,21 @@ use proconio::{fastout, input, marker::*};
 fn main() {
     input! {
         n: usize,
-        a: [usize; n],
+        m: usize,
+        s: [String; n],
+        t: [String; m],
     };
+
+    let mut si = 0;
+    let mut ti = 0;
+    while si < n {
+        if s[si] == t[ti] {
+            println!("Yes");
+            si += 1;
+            ti += 1;
+        } else {
+            println!("No");
+            si += 1;
+        }
+    }
 }
