d32a9ce2eac6674ba17f648e05015af4cf6ee186
zonkyy <zonk.yy30@gmail.com>
2024-04-17 15:06:51 +0900

    adt_all_20240404_3 の g 解答

--- adt_all_20240404_3/src/bin/g.rs
+++ adt_all_20240404_3/src/bin/g.rs
@@ -11,18 +11,19 @@ fn main() {
         a: [usize; n],
     };
 
-    let mut m = HashMap::new();
-    for i in a {
-        *m.entry(i).or_insert(0) += 1;
+    const UPPER: usize = 2 * 10usize.pow(5) + 1;
+    let mut v = vec![0; UPPER];
+
+    for i in a.iter() {
+        v[*i] += 1;
+    }
+    for i in 1..UPPER {
+        v[i] += v[i - 1];
     }
 
     let mut ans = 0;
-    for comb in m.keys().combinations(3) {
-        let mut tmp = 1;
-        for c in comb {
-            tmp *= m[c];
-        }
-        ans += tmp;
+    for i in 0..n {
+        ans += v[a[i] - 1] * (n - v[a[i]]);
     }
 
     println!("{}", ans);
