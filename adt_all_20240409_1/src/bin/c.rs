b6c344d73d7f743518045fae517d8d9dc10e805d
zonkyy <zonk.yy30@gmail.com>
2024-04-18 12:45:38 +0900

    adt_all_20240409_1 の e まで解答

--- adt_all_20240409_1/src/bin/c.rs
+++ adt_all_20240409_1/src/bin/c.rs
@@ -3,7 +3,45 @@ use proconio::{fastout, input, marker::*};
 #[fastout]
 fn main() {
     input! {
-        n: usize,
-        a: [usize; n],
+        mut s: Chars,
+        mut t: Chars,
     };
+    s.sort();
+    let s = s.iter().collect::<String>();
+    t.sort();
+    let t = t.iter().collect::<String>();
+
+    let sd = match s.as_str() {
+        "AB" => 1,
+        "AC" => 2,
+        "AD" => 2,
+        "AE" => 1,
+        "BC" => 1,
+        "BD" => 2,
+        "BE" => 2,
+        "CD" => 1,
+        "CE" => 2,
+        "DE" => 1,
+        _ => unreachable!(),
+    };
+
+    let td = match t.as_str() {
+        "AB" => 1,
+        "AC" => 2,
+        "AD" => 2,
+        "AE" => 1,
+        "BC" => 1,
+        "BD" => 2,
+        "BE" => 2,
+        "CD" => 1,
+        "CE" => 2,
+        "DE" => 1,
+        _ => unreachable!(),
+    };
+
+    if sd == td {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
 }
--- adt_all_20240409_1/src/bin/e.rs
+++ adt_all_20240409_1/src/bin/e.rs
@@ -4,6 +4,20 @@ use proconio::{fastout, input, marker::*};
 fn main() {
     input! {
         n: usize,
-        a: [usize; n],
+        ab: [(usize, usize); n],
     };
+
+    let mut ab = ab
+        .iter()
+        .enumerate()
+        .map(|(i, (a, b))| (i + 1, a, a + b))
+        .collect::<Vec<_>>();
+    ab.sort_by(|a, b| (b.1 * a.2).cmp(&(a.1 * b.2)));
+    println!(
+        "{}",
+        ab.iter()
+            .map(|(i, _, _)| i.to_string())
+            .collect::<Vec<_>>()
+            .join(" ")
+    );
 }
