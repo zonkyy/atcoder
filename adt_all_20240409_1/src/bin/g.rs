f8c8bcfab31ede71ecc63452a91c38ecaf0e2b8c
zonkyy <zonk.yy30@gmail.com>
2024-04-19 10:34:24 +0900

    adt_all_20240409_1 の g まで解答

--- adt_all_20240409_1/src/bin/g.rs
+++ adt_all_20240409_1/src/bin/g.rs
@@ -1,9 +1,40 @@
+use std::cmp;
+
 use proconio::{fastout, input, marker::*};
+
+fn f(a: u128, b: u128) -> u128 {
+    a * a * a + a * a * b + a * b * b + b * b * b
+}
 
 #[fastout]
 fn main() {
     input! {
-        n: usize,
-        a: [usize; n],
+        n: u128,
     };
+
+    let mut upper = 0;
+    while upper * upper * upper < n {
+        upper += 1;
+    }
+    let mut l = 0;
+    let mut r = upper;
+    let mut min = upper * upper * upper;
+    while l < upper {
+        while f(l, r) > n {
+            r -= 1;
+        }
+        min = cmp::min(min, f(l, r + 1));
+
+        while f(l, r) < n {
+            l += 1;
+        }
+        min = cmp::min(min, f(l, r));
+
+        if f(l, r) == n {
+            min = n;
+            break;
+        }
+    }
+
+    println!("{}", min);
 }
