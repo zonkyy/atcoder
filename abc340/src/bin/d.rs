cb2d9c0a040623e59a7196aab262b32000704039
zonkyy <zonk.yy30@gmail.com>
2024-04-24 13:45:49 +0900

    dp を構成していたけど間違いに気付いた

--- abc340/src/bin/d.rs
+++ abc340/src/bin/d.rs
@@ -4,6 +4,18 @@ use proconio::{fastout, input, marker::*};
 fn main() {
     input! {
         n: usize,
-        a: [usize; n],
+        mut abx: [(usize, usize, Usize1); n-1],
     };
+    const UPPER: usize = usize::MAX;
+    abx.sort_by_key(|x| x.2);
+
+    let mut dp = vec![UPPER; n];
+    dp[0] = 0;
+
+    for (i, &(a, b, x)) in abx.iter().enumerate() {
+        dp[i + 1] = dp[i + 1].min(dp[i] + a);
+        dp[x] = dp[x].min(dp[i] + b);
+    }
+
+    println!("{:?}", dp);
 }
