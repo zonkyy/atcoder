4ff965388c91be9fa2f459dbe46a36fac7b53e3a
zonkyy <zonk.yy30@gmail.com>
2024-04-24 12:52:26 +0900

    abc340 の c を解答

--- abc340/src/bin/c.rs
+++ abc340/src/bin/c.rs
@@ -1,7 +1,27 @@
 use std::collections::HashMap;
 
-use num_integer::div_ceil;
 use proconio::{fastout, input, marker::*};
+
+fn dfs(memo: &mut HashMap<u128, u128>, n: u128) -> u128 {
+    if let Some(&i) = memo.get(&n) {
+        return i;
+    }
+
+    let mut ans = 0;
+    let ceil = n / 2 + if n % 2 == 0 { 0 } else { 1 };
+    let floor = n / 2;
+
+    if ceil >= 2 {
+        ans += dfs(memo, ceil);
+    }
+    if floor >= 2 {
+        ans += dfs(memo, floor);
+    }
+    ans += n;
+
+    memo.insert(n, ans);
+    ans
+}
 
 #[fastout]
 fn main() {
@@ -9,32 +29,6 @@ fn main() {
         n: u128,
     };
 
-    let mut v = vec![n];
     let mut memo = HashMap::new();
-
-    let mut ans = 0;
-    while let Some(i) = v.pop() {
-        if let Some(&(ceil, floor)) = memo.get(&i) {
-            if ceil >= 2 {
-                v.push(ceil);
-            }
-            if floor >= 2 {
-                v.push(floor);
-            }
-            ans += i;
-        } else {
-            let ceil = i / 2 + if i % 2 == 0 { 0 } else { 1 };
-            let floor = i / 2;
-            memo.insert(i, (ceil, floor));
-            if ceil >= 2 {
-                v.push(ceil);
-            }
-            if floor >= 2 {
-                v.push(floor);
-            }
-            ans += i;
-        }
-    }
-
-    println!("{}", ans);
+    println!("{}", dfs(&mut memo, n));
 }
