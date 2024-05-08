f540678b87bf1d8f9d043dc7219e27fcf79d6cbd
zonkyy <zonk.yy30@gmail.com>
2024-05-08 14:37:30 +0900

    abc338 の c まで解答

--- abc338/src/bin/a.rs
+++ abc338/src/bin/a.rs
@@ -3,7 +3,20 @@ use proconio::{fastout, input, marker::*};
 #[fastout]
 fn main() {
     input! {
-        n: usize,
-        a: [usize; n],
+        s: Chars,
     };
+
+    if !('A'..='Z').contains(&s[0]) {
+        println!("No");
+        return;
+    }
+
+    for i in 1..(s.len()) {
+        if !('a'..='z').contains(&s[i]) {
+            println!("No");
+            return;
+        }
+    }
+
+    println!("Yes");
 }
--- abc338/src/bin/c.rs
+++ abc338/src/bin/c.rs
@@ -4,6 +4,41 @@ use proconio::{fastout, input, marker::*};
 fn main() {
     input! {
         n: usize,
+        q: [usize; n],
         a: [usize; n],
+        b: [usize; n],
     };
+    const INF: usize = usize::MAX;
+
+    let mut a_cnt_max = INF;
+    let mut b_cnt_max = INF;
+    for i in 0..n {
+        if a[i] != 0 {
+            a_cnt_max = a_cnt_max.min(q[i] / a[i]);
+        }
+        if b[i] != 0 {
+            b_cnt_max = b_cnt_max.min(q[i] / b[i]);
+        }
+    }
+
+    let mut a_cnt = a_cnt_max + 1;
+    let mut b_cnt = 0;
+    let mut ans = a_cnt_max;
+    while a_cnt > 0 {
+        a_cnt -= 1;
+
+        'cp: loop {
+            b_cnt += 1;
+            for i in 0..n {
+                if a[i] * a_cnt + b[i] * b_cnt > q[i] {
+                    b_cnt -= 1;
+                    break 'cp;
+                }
+            }
+        }
+
+        ans = ans.max(a_cnt + b_cnt);
+    }
+
+    println!("{}", ans);
 }
