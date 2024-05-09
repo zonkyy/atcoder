e60fbab23d5bfc3546511214b1f2934673087f2a
zonkyy <zonk.yy30@gmail.com>
2024-05-09 13:10:04 +0900

    abc352 の d を解答

--- abc352/src/bin/d.rs
+++ abc352/src/bin/d.rs
@@ -1,9 +1,31 @@
+use std::collections::BTreeSet;
+
 use proconio::{fastout, input, marker::*};
 
 #[fastout]
 fn main() {
     input! {
         n: usize,
-        a: [usize; n],
+        k: usize,
+        p: [usize; n],
     };
+
+    let mut q = vec![0; n];
+    for (idx, &i) in p.iter().enumerate() {
+        q[i - 1] = idx;
+    }
+
+    let mut buf = BTreeSet::new();
+    for &i in &q[0..k] {
+        buf.insert(i);
+    }
+
+    let mut ans = buf.last().unwrap() - buf.first().unwrap();
+    for i in 0..(n - k) {
+        buf.remove(&q[i]);
+        buf.insert(q[i + k]);
+        ans = ans.min(buf.last().unwrap() - buf.first().unwrap());
+    }
+
+    println!("{}", ans);
 }
