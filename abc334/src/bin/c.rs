c04479c325bfb37d4fa7efbb43a4118263c5777e
zonkyy <zonk.yy30@gmail.com>
2024-05-17 16:18:41 +0900

    abc334 の d まで解答

--- abc334/src/bin/c.rs
+++ abc334/src/bin/c.rs
@@ -1,3 +1,51 @@
+use std::cmp;
+
+use proconio::{fastout, input, marker::*};
+
+#[fastout]
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        k: usize,
+        a: [usize; k]
+    };
+
+    if k == 1 {
+        println!("0");
+        return;
+    }
+
+    let mut lv = vec![0; k / 2];
+    let mut acc = 0;
+    for i in 0..(k / 2) {
+        acc += a[i * 2 + 1] - a[i * 2];
+        lv[i] = acc;
+    }
+
+    if k % 2 == 0 {
+        println!("{}", acc);
+        return;
+    }
+
+    let mut rv = vec![0; k / 2];
+    acc = 0;
+    for i in 0..(k / 2) {
+        acc += a[k - i * 2 - 1] - a[k - i * 2 - 2];
+        rv[k / 2 - i - 1] = acc;
+    }
+
+    let mut ans = *cmp::min(lv.last(), rv.first()).unwrap();
+    for i in 0..(k / 2 - 1) {
+        ans = ans.min(lv[i] + rv[i + 1]);
+    }
+
+    println!("{}", ans);
 }
+
+// 1 2 4 5 7 8 12 15 16 19 20
+
+//  1   1   1    3     3
+//  1   2   3    6     9
+
+//    2   2   4     1     1
+//   10   8   6     2     1
--- abc334/src/bin/d.rs
+++ abc334/src/bin/d.rs
@@ -1,3 +1,24 @@
+use proconio::{fastout, input, marker::*};
+
+#[fastout]
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        q: usize,
+        mut r: [usize; n],
+        queries: [usize; q],
+    };
+    r.sort();
+
+    let mut acc = Vec::new();
+    let mut tmp = 0;
+    for &i in r.iter() {
+        tmp += i;
+        acc.push(tmp);
+    }
+
+    for &i in queries.iter() {
+        let ans = acc.partition_point(|&x| x <= i);
+        println!("{}", ans);
+    }
 }
