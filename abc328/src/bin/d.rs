0ef2eb15168e8c8b585b6ca609500d61fdfdaf2d
zonkyy <zonk.yy30@gmail.com>
2024-05-20 15:31:08 +0900

    abc328 の d が TLE

--- abc328/src/bin/d.rs
+++ abc328/src/bin/d.rs
@@ -1,9 +1,33 @@
+use std::{cmp, collections::BTreeSet};
+
 use proconio::{fastout, input, marker::*};
 
 #[fastout]
 fn main() {
     input! {
-        n: usize,
-        a: [usize; n],
+        s: Chars,
     };
+
+    let mut set = (0..s.len()).collect::<BTreeSet<_>>();
+    let mut idx = 0;
+    while set.len() >= 2 && idx < set.len() - 2 {
+        let a = *set.iter().nth(idx).unwrap();
+        let b = *set.iter().nth(idx + 1).unwrap();
+        let c = *set.iter().nth(idx + 2).unwrap();
+        if s[a] == 'A' && s[b] == 'B' && s[c] == 'C' {
+            set.remove(&a);
+            set.remove(&b);
+            set.remove(&c);
+            idx = if idx >= 2 { idx - 2 } else { 0 };
+        } else {
+            idx += 1;
+        }
+    }
+
+    for &i in set.iter() {
+        print!("{}", s[i]);
+    }
+    if set.len() > 0 {
+        println!();
+    }
 }
