4531e89de4497d4bc015557b13c92e53a6eea9e1
zonkyy <zonk.yy30@gmail.com>
2024-05-20 09:55:20 +0900

    abc331 の d まで解答

--- abc331/src/bin/a.rs
+++ abc331/src/bin/a.rs
@@ -3,7 +3,24 @@ use proconio::{fastout, input, marker::*};
 #[fastout]
 fn main() {
     input! {
-        n: usize,
-        a: [usize; n],
+        m: usize,
+        d: usize,
+        mut yy: usize,
+        mut mm: usize,
+        mut dd: usize,
     };
+
+    dd += 1;
+
+    if dd > d {
+        mm += 1;
+        dd = 1;
+    }
+
+    if mm > m {
+        yy += 1;
+        mm = 1;
+    }
+
+    println!("{} {} {}", yy, mm, dd);
 }
