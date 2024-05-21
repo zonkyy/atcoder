9404c669f52af0e28ab3674bc37bf09a3cd88e15
zonkyy <zonk.yy30@gmail.com>
2024-05-21 18:06:33 +0900

    abc327 の d を Two SAT で解答

--- abc327/src/bin/d.rs
+++ abc327/src/bin/d.rs
@@ -1,5 +1,4 @@
-use std::collections::HashSet;
-
+use ac_library::TwoSat;
 use proconio::{fastout, input, marker::*};
 
 #[fastout]
@@ -11,52 +10,11 @@ fn main() {
         b: [Usize1; m],
     };
 
-    let mut adj = vec![HashSet::<usize>::new(); n];
-    let mut unchecked = HashSet::new();
-    for (&ai, &bi) in a.iter().zip(b.iter()) {
-        adj[ai].insert(bi);
-        adj[bi].insert(ai);
-        unchecked.insert(ai);
-        unchecked.insert(bi);
+    let mut ts = TwoSat::new(n);
+    for (&a, &b) in a.iter().zip(b.iter()) {
+        ts.add_clause(a, true, b, true);
+        ts.add_clause(a, false, b, false);
     }
 
-    let mut numbers = vec![-1; n];
-    let mut queue = Vec::new();
-    queue.push(unchecked.take(&a[0]).unwrap());
-    while !(unchecked.is_empty() && queue.is_empty()) {
-        // 次に処理するノード取得
-        let next_node;
-        if let Some(node) = queue.pop() {
-            next_node = node;
-        } else {
-            let node = *unchecked.iter().next().unwrap();
-            next_node = unchecked.take(&node).unwrap();
-        }
-
-        // まだ数字が決まってないなら 0 を設定
-        if numbers[next_node] == -1 {
-            numbers[next_node] = 0;
-        }
-
-        // 次のノードと隣接しているノードの数字が矛盾したら終了
-        // 矛盾していないものは queue に入れていく
-        for &nextto in adj[next_node].iter() {
-            if numbers[nextto] == numbers[next_node] {
-                println!("No");
-                return;
-            }
-
-            if numbers[nextto] == -1 {
-                numbers[nextto] = 1 - numbers[next_node];
-            }
-
-            if unchecked.contains(&nextto) {
-                queue.push(nextto);
-                unchecked.take(&nextto);
-                numbers[nextto] = 1 - numbers[next_node];
-            }
-        }
-    }
-
-    println!("Yes");
+    println!("{}", if ts.satisfiable() { "Yes" } else { "No" });
 }
