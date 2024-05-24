175880eea4fbf2305416c027cb461e8f234ba969
zonkyy <zonk.yy30@gmail.com>
2024-05-24 14:54:18 +0900

    abc324 の d を解答

--- abc324/src/bin/d.rs
+++ abc324/src/bin/d.rs
@@ -27,6 +27,33 @@ impl<Iter: IntoIterator> Iterator for Transposed<Iter> {
 fn main() {
     input! {
         n: usize,
-        a: [usize; n],
+        s: String,
     };
+    const UPPER: usize = 3162300;
+
+    if s == "0".to_string() {
+        println!("1");
+        return;
+    }
+
+    // (1～9 のソート、0 の数)
+    let mut sqs = Vec::new();
+    for i in 1..UPPER {
+        let s_num = (i * i).to_string();
+        let mut s_num_nonzero: Vec<char> = s_num.replace("0", "").chars().collect();
+        s_num_nonzero.sort();
+        let s_num_nonzero: String = s_num_nonzero.into_iter().collect();
+        let zero_cnt = s_num.len() - s_num_nonzero.len();
+        sqs.push((s_num_nonzero, zero_cnt));
+    }
+
+    let mut s_nonzero = s.replace("0", "").chars().collect::<Vec<_>>();
+    s_nonzero.sort();
+    let s_nonzero = s_nonzero.into_iter().collect::<String>();
+    let s_zero_cnt = n - s_nonzero.len();
+    let ans = sqs
+        .iter()
+        .filter(|x| x.0 == s_nonzero && x.1 <= s_zero_cnt)
+        .count();
+    println!("{}", ans);
 }
